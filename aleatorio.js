f91e2a403bcc8effd25e6b4f44afc27b2e3d0a02
Kamilla Vitoria <117935629+myllavitoria@users.noreply.github.com>
2024-08-22 11:40:03 -0300

    Create aleatorio.js

---
+++ aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Kamilla"];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
